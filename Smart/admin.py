7469262dd12d86338aab414cf401884a95c393ed
tegratisth <tegratshibanda20@gmail.com>
2025-01-09 13:40:54 +0100

    smartlawfirm

--- Smart/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Area, Title, Service, Contact
-
-
-admin.site.register(Area)
-admin.site.register(Title)
-admin.site.register(Service)
-admin.site.register(Contact)
